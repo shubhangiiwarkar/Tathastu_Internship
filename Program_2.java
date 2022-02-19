a68a9967242ac6022e2c1375a6490efc0cf8a80d
shubhangiiwarkar <73748217+shubhangiiwarkar@users.noreply.github.com>
2022-02-19 09:53:49 +0530

    Create Program_2.java

---
+++ Program_2.java
@@ -0,0 +1,18 @@
+//Write a java program to calculate fibonacci series up to n numbers
+import java.io.*;
+import java.util.Scanner;
+class FibonacciExample1{  
+public static void main(String args[])  
+{    
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);
+    
+ for(i=2;i<count;++i) 
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }    
+  
+}}  
