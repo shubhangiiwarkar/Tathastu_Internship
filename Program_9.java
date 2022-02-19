196ded7ef66bbd08b41b18e5ad2cc3b362f545ae
shubhangiiwarkar <73748217+shubhangiiwarkar@users.noreply.github.com>
2022-02-19 11:04:19 +0530

    Create Program_9.java

---
+++ Program_9.java
@@ -0,0 +1,18 @@
+//write a java program to remove elements from an arraylist.
+import java.util.ArrayList;
+import java.util.List;
+public class GFG {
+	public static void main(String[] args)
+	{
+		List<Integer> al = new ArrayList<>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(1);
+		al.add(2);
+		System.out.println(al);
+		al.remove(1);
+		al.remove(1);
+		System.out.println(al);
+	}
+}
