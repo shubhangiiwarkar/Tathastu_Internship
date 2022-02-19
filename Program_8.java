e0014c22c7ea4b0e97dd8182f8c3ebf92fa3b067
shubhangiiwarkar <73748217+shubhangiiwarkar@users.noreply.github.com>
2022-02-19 10:42:06 +0530

    Create Program_8.java

---
+++ Program_8.java
@@ -0,0 +1,19 @@
+//Write a java program to check whether the given array is mirror inverse or not.
+public class GFG {
+	static boolean isMirrorInverse(int arr[])
+	{
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[arr[i]] != i)
+				return false;
+		}
+		return true;
+	}
+	public static void main(String[] args)
+	{
+		int arr[] = { 1, 2, 3, 0 };
+		if (isMirrorInverse(arr))
+			System.out.println("Yes");
+		else
+			System.out.println("No");
+	}
+}
