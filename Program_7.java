75fcae4001ec548ea9e487153beed0ee5fed58d1
shubhangiiwarkar <73748217+shubhangiiwarkar@users.noreply.github.com>
2022-02-19 10:36:14 +0530

    Create Program_7.java

---
+++ Program_7.java
@@ -0,0 +1,15 @@
+//Write a java program to reverse the letters present in the given string.
+import java.util.*;
+import java.io.*;
+public class StringFormatter {  
+public static String reverseWord(String str){  
+    String words[]=str.split("\\s");  
+    String reverseWord="";  
+    for(String w:words){  
+        StringBuilder sb=new StringBuilder(w);  
+        sb.reverse();  
+        reverseWord+=sb.toString()+" ";  
+    }  
+    return reverseWord.trim();  
+}  
+}  
