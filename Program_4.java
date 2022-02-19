5d7b94ca333d339cd541a5a27da4eaefabe53479
shubhangiiwarkar <73748217+shubhangiiwarkar@users.noreply.github.com>
2022-02-19 09:59:04 +0530

    Create Program_4.java

---
+++ Program_4.java
@@ -0,0 +1,26 @@
+//Write a java program to find out whether the given string is palindrome or not.
+import java.util.*;
+import java.io.*;
+public class Palindrome
+{
+    public static void main(String args[])
+    {
+        String a, b = "";
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the string you want to check:");
+        a = s.nextLine();
+        int n = a.length();
+        for(int i = n - 1; i >= 0; i--)
+        {
+            b = b + a.charAt(i);
+        }
+        if(a.equalsIgnoreCase(b))
+        {
+            System.out.println("The string is palindrome.");
+        }
+        else
+        {
+            System.out.println("The string is not palindrome.");
+        }
+    }
+}
